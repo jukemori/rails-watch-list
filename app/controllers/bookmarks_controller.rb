62773ed05c050b7a5a760da8b08b16972d58c8dd
Jun Ukemori <jukemori@gmail.com>
2023-05-12 10:52:15 +0900

    style added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,9 +8,13 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
