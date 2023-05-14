693a667233b96e6fc7daad9035a39216c5de4726
Jun Ukemori <jukemori@gmail.com>
2023-05-14 15:21:11 +0900

    show page changed

--- app/javascript/nav_bar.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector('.navbar-lewagon');
-
-window.addEventListener('scroll', function() {
-  if (window.scrollY > 0) {
-    navbar.classList.add('hidden');
-  } else {
-    navbar.classList.remove('hidden');
-  }
-});
